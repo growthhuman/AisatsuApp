f37d09679ed946e407050e6d367413385d98a020
growthhuman <growthhuman@gmail.com>
2017-08-21 23:39:24 +0900

    space change

--- app/src/main/java/jp/techacademy/kenta/imabayashi/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/kenta/imabayashi/aisatsuapp/MainActivity.java
@@ -2,14 +2,30 @@ package jp.techacademy.kenta.imabayashi.aisatsuapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_main);
 
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.button1 ){
+            showTimePickerDialog();
+        }
+    }
+
+    private void showTimePickerDialog() {
     }
 }
