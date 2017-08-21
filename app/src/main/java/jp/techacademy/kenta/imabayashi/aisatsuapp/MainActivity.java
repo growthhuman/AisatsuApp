5fa22b8662a31195a6b9dff572711b28acb88f5d
growthhuman <growthhuman@gmail.com>
2017-08-21 23:43:30 +0900

    space change

--- app/src/main/java/jp/techacademy/kenta/imabayashi/aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/kenta/imabayashi/aisatsuapp/MainActivity.java
@@ -1,9 +1,12 @@
 package jp.techacademy.kenta.imabayashi.aisatsuapp;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,5 +30,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                    }
+                },
+                13, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
     }
 }
